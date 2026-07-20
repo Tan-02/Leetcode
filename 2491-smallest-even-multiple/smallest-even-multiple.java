e8e382102204f99806a0d963646cad557e41df2d
Tanishka <tanishu075@gmail.com>
2026-07-20 11:19:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (97.25%) - LeetSync

---
+++ 2491-smallest-even-multiple/smallest-even-multiple.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        int ans=0;
+        for(int i=n;i<=2*n;i++ ){
+        if(i%2==0 && i%n==0){
+       return i;
+        }
+        }
+        return -1;
+    }
+}
