33110a4c8fdb1f8d6e3aedb751f834c31a78df3a
Tanishka <tanishu075@gmail.com>
2026-07-20 10:00:43 +0530

    Time: 3 ms (10.49%) | Memory: 43.2 MB (11.26%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String toLowerCase(String s) {
+        String ans="";
+    for (int i = 0; i < s.length(); i++) {
+        ans+= Character.toLowerCase(s.charAt(i));
+    }
+    return ans;       
+    }
+}
