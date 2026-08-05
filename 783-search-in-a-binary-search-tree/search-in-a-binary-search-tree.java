22f5939d28e18d37af539c848b3d11c29fa58e4e
Tanishka <tanishu075@gmail.com>
2026-08-05 23:28:45 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (89.39%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -21,8 +21,8 @@ class Solution {
         if(root.val==val){
             return root;
         }
-        else if(val>root.val){
-            return searchBST(root.right, val);
+        if(val>root.val){
+            return searchBST(root.right,val);
         }
         else{
             return searchBST(root.left,val);
